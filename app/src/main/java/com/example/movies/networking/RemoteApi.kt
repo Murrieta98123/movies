af8a36e2799b76ce7d149be197dd95a533356525
juan manuel <helo201178@hotmail.com>
2021-11-19 17:18:27 -0600

    Código modificado

--- app/src/main/java/com/example/movies/networking/RemoteApi.kt
+++ app/src/main/java/com/example/movies/networking/RemoteApi.kt
@@ -1,12 +1,21 @@
 package com.example.movies.networking
 
 import com.example.movies.models.MovieResponse
+val key = "55957fcf3ba81b137f8fc01ac5a31fb5"
+val language = "en-US"
 
 class RemoteApi(private val apiService: RemoteApiService) {
     suspend fun getMovies(): Result<MovieResponse> = try {
         val data = apiService.getMovies()
         Success(data)
     } catch (error: Throwable) {
+        Failure(error)
+    }
+
+    suspend fun getMoviesByPage(page:Int): Result<MovieResponse> = try {
+        var data = apiService.getMoviesByPage(page,key,language)
+        Success(data)
+    }catch (error: Throwable) {
         Failure(error)
     }
 }
